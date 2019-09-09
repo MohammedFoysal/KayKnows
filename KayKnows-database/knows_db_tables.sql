9d468605390fa68d6c631b04458946b7d3bf317c
Aleksandra <aleksandra@Numberwang.kainos.com>
2019-09-09 16:31:47 +0100

    Added description field in roles table

--- KayKnows-database/knows_db_tables.sql
+++ KayKnows-database/knows_db_tables.sql
@@ -25,6 +25,7 @@ CREATE TABLE IF NOT EXISTS roles(
     role_id SMALLINT UNSIGNED PRIMARY KEY auto_increment,
     role_name VARCHAR(100) NOT NULL,
     role_spec VARCHAR(300) NULL,
+    role_description VARCHAR(500) NULL,
     capability_id SMALLINT UNSIGNED,
     family_id SMALLINT UNSIGNED,
     band_id SMALLINT UNSIGNED,
