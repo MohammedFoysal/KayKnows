0b6233e7d097aba93c22e71fbdac798d8d4a6dd8
Lewis Birks <lewisb@kainos.com>
2019-09-09 16:40:49 +0100

    Made basic connection in express

---
+++ KayKnows-backend/db.js
@@ -0,0 +1,16 @@
+require('dotenv').config({path: 'mysql.env'});
+
+const mysql = require('mysql');
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE
+});
+
+db.connect(function (err) {
+  if (err) {
+    throw err;
+  }
+  console.log("Connected to MySQL");
+});
---
+++ KayKnows-backend/index.js
@@ -0,0 +1,34 @@
+const start = module.exports = function makeServer() {
+
+  const express = require('express');
+  const app = express();
+  const bodyParser = require('body-parser');
+  const {check, validationResult, body} = require('express-validator');
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded());
+
+  const db = require('./db.js');
+
+  function handleError(err, req, res) {
+    if (err.errno === 3819) {
+      err.code = "ER_CHECK_CONSTRAINT_VIOLATED";
+    }
+    //  console.error(`${err.errno} (${err.code}) : ${err.sqlMessage}`);
+    res.status(500).send({
+      message: err
+    });
+  }
+
+  const server = app.listen(8002, function () {
+    console.log('express started on port 8002');
+  });
+
+
+
+  return server;
+};
+
+if (!process.env.NODE_ENV) {
+  start();
+}
