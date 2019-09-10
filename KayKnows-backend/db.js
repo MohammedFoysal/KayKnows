4c47311e2d2f0883567fc33c6ea9e38603b2fd71
AkaJSaunders <julianoasaunders@gmail.com>
2019-09-10 11:57:34 +0100

    fixed the createConnection if else statemement

--- KayKnows-backend/db.js
+++ KayKnows-backend/db.js
@@ -6,18 +6,18 @@ const db;
 
 if (process.env.NODE_ENV) {
   db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
-  });
-}
-else{
-  db = mysql.createConnection({
     host: process.env.TEST_DB_HOST,
     user: process.env.TEST_DB_USER,
     password: process.env.TEST_DB_PASS,
     database: process.env.TEST_DB_DATABASE
+  });
+}
+else{
+  db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DATABASE
   });
 }
 
