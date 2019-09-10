ea430e3267acd29ac216c0173a698a4a3855a32d
Lewis Birks <lewisb@kainos.com>
2019-09-10 09:38:06 +0100

    Added untested functions to get families and capabilities from the database

--- KayKnows-backend/db.js
+++ KayKnows-backend/db.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path: 'mysql.env'});
 
 const mysql = require('mysql');
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -8,9 +9,30 @@ const db = mysql.createConnection({
   database: process.env.DB_DATABASE
 });
 
-db.connect(function (err) {
+db.connect(err => {
   if (err) {
     throw err;
   }
   console.log("Connected to MySQL");
 });
+
+exports.getCapabilities = function (callback) {
+  db.query('SELECT * FROM capabilities',
+      (error, rows) => {
+        if (error) {
+          return callback(error, null);
+        }
+        return callback(null, rows);
+      })
+};
+
+exports.getFamilies = function (callback) {
+  db.query('SELECT * FROM families',
+      (error, rows) => {
+        if (error) {
+          return callback(error, null);
+        }
+        callback(null, rows);
+      })
+};
+
--- KayKnows-backend/index.js
+++ KayKnows-backend/index.js
@@ -3,7 +3,7 @@ const start = module.exports = function makeServer() {
   const express = require('express');
   const app = express();
   const bodyParser = require('body-parser');
-  const {check, validationResult, body} = require('express-validator');
+  const {check, validationResult, body} = require('express-validator'); // Later use
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded());
@@ -14,13 +14,30 @@ const start = module.exports = function makeServer() {
     if (err.errno === 3819) {
       err.code = "ER_CHECK_CONSTRAINT_VIOLATED";
     }
+
+    console.error(`${err.errno} (${err.code}) : ${err.sqlMessage}`);
+
     res.status(500).send({
-      message: err
+      message: err.sqlMessage
     });
   }
 
   const server = app.listen(8002, function () {
     console.log('express started on port 8002');
+  });
+
+  app.get('/capabilities', (req, res) => {
+    db.getCapabilities((error, rows) => {
+      if (error) return handleError(error, req, res);
+      res.send(rows)
+    })
+  });
+
+  app.get('/families', (req, res) => {
+    db.getFamilies((error, rows) => {
+      if (error) return handleError(error, req, res);
+      res.send(rows)
+    })
   });
 
   return server;
