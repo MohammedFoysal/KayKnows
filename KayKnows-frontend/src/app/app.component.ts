308e18ba9b2daadd4a1e20f43ac70f434ffe867f
Mohammed Foysal <mohammedfoysal@North.local>
2019-09-10 09:35:58 +0100

    Implemented custom org chart

--- KayKnows-frontend/src/app/app.component.ts
+++ KayKnows-frontend/src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'KayKnows-frontend';
+
+  data = [
+    {id: 1, label: 'somethinbg', opened: true, children: [
+      {id: 2, opened: false, label: 'child1', children: [
+        {id: 3, opened: false,  label: 'grandchild1', children: []},
+        {id: 4, opened: false, label: 'grandchild2', children: []}
+      ]},
+      {id: 5, opened: false, label: 'child2', children: [
+        {id: 7, opened: false, label: 'grandchild3', children: []},
+        {id: 8, opened: false, label: 'grandchild4', children: []}
+      ]},
+      {id: 6, opened: false, label: 'child3', children: [
+        {id: 9, opened: false, label: 'grandchild7', children: []},
+        {id: 10, opened: false, label: 'grandchild8', children: []}
+      ]},
+    ]}
+  ];
 }
