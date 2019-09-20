52b43f372d1c31474a20528a8d9bf396f242d10e
Lewis Birks <22620804+lewisbirks@users.noreply.github.com>
2019-09-20 11:47:07 +0100

    Add capability lead

--- KayKnows-backend/db.js
+++ KayKnows-backend/db.js
@@ -1,11 +1,11 @@
-require('dotenv').config({ path: 'config.env' });
+require('dotenv').config({path: 'config.env'});
+
 const util = require('util');
 const mysql = require('mysql');
 var log4js = require('log4js');
 var logger = log4js.getLogger();
 logger.level = 'debug';
 const env = process.env.NODE_ENV === 'test' || 'dev';
-
 const connection = (env === 'dev') ? process.env.DB_DATABASE :
     process.env.DB_TEST_DATABASE;
 
@@ -190,6 +190,10 @@ exports.removeBand = async (band_id) => {
 
 exports.addCapability = async(capability) => {
   return await query("INSERT INTO capabilities SET ?", [capability]);
+};
+
+exports.addCapabilityLead =  async(capLead) => {
+  return await query("INSERT INTO capability_leads SET ?", [capLead]);
 };
 
 exports.getCapabilitiesByFamIdAndCapName = async(family_id, capability_name) => {
